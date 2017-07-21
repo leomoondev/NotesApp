5305a579233fb663ddbe378ba9e79aff1b426b79
Leo Moon <leom@skysecure.com>
2017-07-21 10:35:42 -0700

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Leo Moon on 2017-07-21.
+//  Copyright © 2017 Leo Moon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
